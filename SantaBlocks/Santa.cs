d35a8e191e3420b79abb24cd0abe57ad0bd504bd
Petar Stanev <petarstanev@outlook.com>
2016-12-28 22:29:18 +0200

    init

---
+++ SantaBlocks/Santa.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SantaBlocks
+{
+    public enum Direction
+    {
+        North = 0, East = 1, South = 2, West = 3
+    }
+
+    public class Santa
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Direction Direction;
+
+        public Santa()
+        {
+            X = 0;
+            Y = 0;
+            Direction = Direction.North;
+        }
+
+        public void Move(char rotate, int moves)
+        {
+            ChangeDirection(rotate);
+            MoveBlocks(moves);
+        }
+
+        private void MoveBlocks(int moves)
+        {
+            switch (Direction)
+            {
+                case Direction.North:
+                    Y += moves;
+                    break;
+                case Direction.South:
+                    Y -= moves;
+                    break;
+                case Direction.East:
+                    X += moves;
+                    break;
+                case Direction.West:
+                    X -= moves;
+                    break;
+            }
+        }
+
+        private void ChangeDirection(char rotate)
+        {
+            int directionInt = (int)Direction;
+
+            if (rotate == 'R')
+            {
+                if (directionInt == 3)
+                {
+                    directionInt = 0;
+                }
+                else
+                {
+                    directionInt++;
+                }
+            }
+            else
+            {
+                if (directionInt == 0)
+                {
+                    directionInt = 3;
+                }
+                else
+                {
+                    directionInt--;
+                }
+            }
+            Direction = (Direction)directionInt;
+        }
+
+        public int BlocksAway()
+        {
+            int blocksX, blocksY;
+            blocksX = GeneratePositiveNumber(X);
+            blocksY = GeneratePositiveNumber(Y);
+            return blocksX + blocksY;
+        }
+
+        private int GeneratePositiveNumber(int input)
+        {
+            if (input < 0)
+            {
+                input = input * -1;
+            }
+
+            return input;
+        }
+
+        public void SetUpRoad(string road)
+        {
+            string[] moves;
+            road = road.Replace(" ", string.Empty);
+            moves = road.Split(',');
+
+            foreach (string move in moves)
+            {
+                char letter = move[0];
+
+                int number = Int32.Parse(move.Substring(1));
+                Move(letter, number);
+            }
+        }
+    }
+}
---
+++ SantaTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SantaBlocks;
+
+
+namespace SantaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Santa santa = new Santa();
+
+
+        [TestMethod]
+        public void TestR2L3()
+        {
+            santa.SetUpRoad("R2, L3");
+            Assert.AreEqual(santa.BlocksAway(), 5);
+        }
+
+        [TestMethod]
+        public void TestR2R2R2()
+        {
+            santa.SetUpRoad("R2, R2, R2");
+            Assert.AreEqual(santa.BlocksAway(), 2);
+        }
+
+        [TestMethod]
+        public void TestR5L5R5R3()
+        {
+            santa.SetUpRoad("R5, L5, R5, R3");
+            Assert.AreEqual(santa.BlocksAway(), 12);
+        }
+
+        [TestMethod]
+        public void TestFinale()
+        {
+            santa.SetUpRoad("L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2");
+            int result = santa.BlocksAway();
+            Assert.AreEqual(santa.BlocksAway(), 279);
+        }
+    }
+}
